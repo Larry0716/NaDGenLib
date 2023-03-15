db3c6ea45b099c8469cc4244ed7803b210f383f7
Larry76 <larry0716@163.com>
2023-03-15 11:14:21 +0800

    modified:   module/graph.h

--- module/graph.h
+++ module/graph.h
@@ -3,9 +3,11 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 
 #include <iostream>
 #include <vector>
+#include <map>
 
 #include "random.h"
 #include "genlib.h"
@@ -13,31 +15,162 @@
 namespace Generator
 {
     using std::vector;
-    using std::unordered_map;
+    using std::map;
     using std::string;
+
+    typedef std::pair<int,int> pii;
+    /**
+     * @brief 图的基类，用于存储图。
+    */
     class Graph{
         public:
-            Graph(){}
-            Graph(){
-                
+            /**
+             * @brief 图的构造函数
+             * @param verCount 图的点数
+             * @param undirectedMap 无向图开关，默认为有向图（false）
+             * @param weightedMap 带权图开关，默认为不带权（false）
+             * @param muiltiedgeCheck 重边检查开关，默认为关闭（false）
+             * @param loopCheck 自环检查开关，默认为关闭（false）
+            */
+            Graph(int verCount,
+                  bool undirectedMap = false, bool weightedMap = false, 
+                  bool muiltiedgeCheck = false, bool loopCheck = false)
+            {
+                ++verCount;
+                head = new int[verCount];
+                id = new int[verCount];
+                --verCount;
+                vertexCount = verCount;
+                undirectedMapSwitch = undirectedMap;
+                muiltiedgeCheckSwitch = muiltiedgeCheck;
+                weightedMapSwitch = weightedMap;
+                loopCheckSwitch = loopCheck;
+                clear();
             }
-            void add_edge(int from, int to,){
+            /**
+             * @brief 用于清空图
+             * @warning 注意，清空图并不意味着清空了其占用的内存空间。
+            */
+            void clear()
+            {
+                for(register int i = 0; i < vertexCount; ++i)
+                    id[i] = i;
+                memset( head, -1, vertexCount * sizeof(int) );
+                ver.clear();
+                next.clear();
+                edge.clear();
+            }
+            /**
+             * @brief 添加一条边
+             * @param from 边的起点
+             * @param to 边的终点
+            */
+            void add_edge(int from, int to)
+            {
+                asserti(!weightedMapSwitch, "PANIC!! The operation doesn't match the setting.");
+                if(!multiedgeChecker(from, to))
+                    return ;
+                if(!loopChecker(from, to))
+                    return ;
+                __add_edge(from, to);
+                if(undirectedMapSwitch)
+                    __add_edge(to, from);
+            }
+            /**
+             * @brief 添加一条带权边
+             * @param from 边的起点
+             * @param to 边的终点
+             * @param weight 边的权
+            */
+            void add_edge(int from, int to, int weight)
+            {
+                asserti(weightedMapSwitch, "PANIC!! The operation doesn't match the setting.");
+                if(!multiedgeChecker(from, to))
+                    return ;
+                if(!loopChecker(from, to))
+                    return ;
+                __add_edge(from, to, weight);
+                if(undirectedMapSwitch)
+                    __add_edge(to, from, weight);
+            }
+            void output(bool shuffleOutput = true){
+                std::ofstream &cout = *rout;
+                if(shuffleOutput)
+                    shuffle();
+                for(int i = 0; i <= vertexCount; ++i){
+                    for(int j = head[i]; ~j; j = next[j]){
+                        cout << id[i] << ' ' << id[ver[j]];
+                        if(weightedMapSwitch)
+                            cout << ' ' << edge[j];
+                        cout << std::endl;
+                    }
+                }
+            }
+        protected:
+            void shuffle(){
+                Random rnd;
+                for(int i = 1; i <= vertexCount; ++i){
+                    int oth = rnd.irand(1,n);
+                    std::swap(id[i],id[oth]);
+                }
+            }
+            inline void __add_edge(int from, int to, int weight)
+            {
+                ++tot;
+                ver.push_back(to);
+                edge.push_back(weight);
+                next.push_back(head[from]);
+                head[from] = next[tot];
+            }
+            inline void __add_edge(int from, int to)
+            {
                 ++tot;
                 ver.push_back(to);
                 next.push_back(head[from]);
                 head[from] = next[tot];
             }
+            /**
+             * @brief 重边检查器
+             * @return true 则为检查通过，false 则为检查失败
+             * @note 若检查失败，则会输出日志
+            */
+            inline bool multiedgeChecker(int from, int to)
+            {
+                if(!muiltiedgeCheckSwitch)
+                    return true;
+                if(from>to)
+                    std::swap(from,to);
+                if(hashtable[{from, to}]) {
+                    fprintf(stderr, "Edge <%d,%d> was unable to pass muiltiedge check. Ignored.\n", from, to);
+                    return false;
+                }
+                hashtable[{from, to}] = true;
+                return true;
+            }
+            inline bool loopChecker(int from, int to)
+            {
+                if(!loopCheckSwitch)
+                    return true;
+                if(from == to){
+                    fprintf(stderr, "Edge <%d,%d> was unable to pass loop check. Ignored.\n", from, to);
+                    return false;
+                }
+                return true;
+            }
         private:
-            vector<int>head;
+            bool loopCheckSwitch;
+            bool muiltiedgeCheckSwitch;
+            bool undirectedMapSwitch;
+            bool weightedMapSwitch;
+            int vertexCount;
+            int tot = 0;
+            int *id;
+            int *head;
             vector<int>next;
             vector<int>edge;
             vector<int>ver;
-            struct EDGE{
-                int from,to;
-            }
-            unordered_map<>
-            int tot = 1;
-    }
+            map<pii,bool> hashtable;
+    };
     /**
      * @brief 生成一颗无根树
     */
