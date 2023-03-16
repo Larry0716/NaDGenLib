7e02b97770a7456f21b7d31af6daebb6128dcbc3
Larry76 <larry0716@163.com>
2023-03-16 10:26:45 +0800

    modified:   module/genlib.h         将 RedirectToFile 该为宏函数，原先的 RedirectToFile 函数         改为 RedirectToFileFunc。 	modified:   module/sequence.h         新增了对排列（Permutation）生成的支持，修复了序列（Sequence）的         一个问题。 	modified:   test/test_import.cpp         更新了对 import 测试的内容 	new file:   test/test_random.cpp         添加了对 Random 的测试 	new file:   test/test_sequence.cpp         添加了对 Sequence 的测试 	new file:   test/test_tree.cpp         添加了对 NoRootTree 的测试

--- test/test_import.cpp
+++ test/test_import.cpp
@@ -2,7 +2,13 @@
 #include "..\genlib.h"
 using namespace std;
 using namespace Generator;
+const int vmin = 114514;
+const int vmax = 1919810;
+void CustomFunction(){
+    AntiSPFA atspfa(10,1e5/10);
+    atspfa.AutoMode(vmin,vmax);
+}
 int main(){
-
-	return 0;
+    AutoGenerate(".\\data\\test%d.in",1,10,CustomFunction);
+    return 0;
 }
